1f6ea11933c3ba54334988013f47b7ced4f4f495
aly8i <ABY00003@students.aust.edu.lb>
2022-11-21 21:14:59 +0200

    cube

---
+++ components/AboutCards.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import styles from '../styles/AboutCards.module.css'
+
+const AboutCards = () => {
+  return (
+    <div className={styles.container1}>
+        <h4>Services</h4>
+        <div className={styles.container2}>
+        <div className={`${styles.flip} ${styles.flipVertical}`}>
+            <div className={`${styles.front} ${styles.first}`} >
+                <h1 className={styles.textShadow}>Actors</h1>
+            </div>
+            <div className={styles.back}>
+            <h2>Actors</h2>
+            <p>Join our proffessional acting community and select performing roles based on previous experiences.</p>
+            </div>
+        </div>
+        <div className={`${styles.flip} ${styles.flipVertical}`}>
+            <div className={`${styles.front} ${styles.second}`}>
+            <h1 className={styles.textShadow}>Production</h1>
+            </div>
+            <div className={styles.back}>
+            <h2>Production</h2>
+            <p>Our production team will handle all the video editing tasks proffesionally, in order to maximize your product.</p>
+            </div>
+        </div>
+        <div className={`${styles.flip} ${styles.flipVertical}`}>
+            <div className={`${styles.front} ${styles.third}`}>
+            <h1 className={styles.textShadow}>Shooting</h1>
+            </div>
+            <div className={styles.back}>
+            <h2>Shooting</h2>
+            <p>Bring your script, your actors, and let our shooting scene transform your words into awesome scenes.</p>
+            </div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default AboutCards
---
+++ components/CubeSlider.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CubeSlider = () => {
+  return (
+    <div class="wrapper">
+        <div class="slider">
+            <div class="container">
+                <div class="slide x"></div>
+                <div class="slide y"></div>
+                <div class="slide z"></div>
+            </div>
+            <div class="shadow"></div>
+        </div>
+    </div>
+  )
+}
+
+export default CubeSlider

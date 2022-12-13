4a32181566155d907cb69f74ec531c509d027480
aly8i <ABY00003@students.aust.edu.lb>
2022-12-14 00:56:18 +0200

    fb auth.3

--- next.config.js
+++ next.config.js
@@ -3,7 +3,15 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images:{
-    domains:["firebasestorage.googleapis.com","hfc-resto.appspot.com","icon-library.com","images.app.goo.gl","scontent.fbey4-2.fna.fbcdn.net","https://scontent.fbey4-2.fna.fbcdn.net","lh3.googleusercontent.com","unsplash.com"]
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fbcdn.net',
+        port: '',
+        pathname: '/v/**',
+      },
+    ],
+    domains:["firebasestorage.googleapis.com","hfc-resto.appspot.com","icon-library.com","images.app.goo.gl","fbcdn.net","https://scontent.fbey4-2.fna.fbcdn.net","lh3.googleusercontent.com","unsplash.com"]
   },
 }
 

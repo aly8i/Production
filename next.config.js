a1b49703af1def65912371000cba7d5d22c19f01
aly8i <ABY00003@students.aust.edu.lb>
2022-12-14 00:40:50 +0200

    fb auth.2

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images:{
-    domains:["firebasestorage.googleapis.com","hfc-resto.appspot.com","icon-library.com","images.app.goo.gl","lh3.googleusercontent.com","unsplash.com"]
+    domains:["firebasestorage.googleapis.com","hfc-resto.appspot.com","icon-library.com","images.app.goo.gl","scontent.fbey4-2.fna.fbcdn.net","https://scontent.fbey4-2.fna.fbcdn.net","lh3.googleusercontent.com","unsplash.com"]
   },
 }
 
